c817db17d0c42a155b944a74fe32eaaa5968f147
Advaith Narayanan <55801439+Glitchyi@users.noreply.github.com>
2021-12-15 23:53:04 +0530

    Add files via upload

---
+++ VROOM.ino
@@ -0,0 +1,154 @@
+#include<DabbleESP32.h>
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
+int INP_B1=2;
+int INP_B2=4;
+int EN_B=6;
+int INP_A1=12;
+int INP_A2=14;
+int EN_A=16;
+
+int Speed0=0;
+int Speed1=37;
+int Speed2=74;
+int Speed3=111;
+int Speed4=148;
+int Speed5=185;
+int Speed6=222;
+int Speed7=255;
+void setup(){
+
+
+  Serial.begin(9600);
+  Dabble.begin("VROOM");
+
+
+  pinMode(EN_A, OUTPUT);  // EN A1
+  pinMode(INP_A1, OUTPUT);  // INP A1
+  pinMode(6, OUTPUT); // INP A1
+
+  pinMode(15,OUTPUT); // EN B1
+  pinMode(4,OUTPUT);  // IN1 B1
+  pinMode(2,OUTPUT);  // IN2 B1
+}
+
+void loop() {
+
+  Dabble.processInput();
+  int xval = GamePad.getXaxisData(); // get x-axis data
+  int yval = GamePad.getYaxisData(); // get y-axis data
+
+  if GamePad.isTrianglePressed(){ // TURN LEFT
+
+    digitalWrite(EN_A, HIGH);  //for controlling the speed of the motor
+
+    digitalWrite(INP_A1, HIGH); //for controlling the direction of the motor
+    digitalWrite(INP_A2, LOW; 
+    
+    //digitalWrite(EN_B, HIGH); //for controlling the speed of the motor
+
+    //digitalWrite(INP_B1, HIGH); //for controlling the direction of the motor
+    //digitalWrite(INP_B2, LOW); 
+    delay(1000);
+
+
+
+  }
+
+  if(yval=0 && xval < 4 && xval > -4){
+
+    digitalWrite(INP_A1, LOW); //for controlling the direction of the motor
+    digitalWrite(INP_A2, LOW; 
+    Serial.write("NO")
+    digitalWrite(INP_B1, LOW); //for controlling the direction of the motor
+    digitalWrite(INP_B2, LOW); 
+
+  }else if ((yval==1 && xval < 4 && xval > -4)){
+
+    analogWrite(EN_A, speed1);  //for controlling the speed of the motor
+
+    digitalWrite(INP_A1, LOW); //for controlling the direction of the motor
+    digitalWrite(INP_A2, HIGH); 
+    
+    analogWrite(EN_B, speed1); //for controlling the speed of the motor
+
+    digitalWrite(INP_B1, HIGH); //for controlling the direction of the motor
+    digitalWrite(INP_B2, LOW); 
+
+  }else if ((yval==2 && xval < 4 && xval > -4)){
+
+    analogWrite(EN_A, speed2);  //for controlling the speed of the motor
+
+    digitalWrite(INP_A1, LOW); //for controlling the direction of the motor
+    digitalWrite(INP_A2, HIGH); 
+    
+    analogWrite(EN_B, speed2); //for controlling the speed of the motor
+
+    digitalWrite(INP_B1, HIGH); //for controlling the direction of the motor
+    digitalWrite(INP_B2, LOW); 
+
+  }else if ((yval==3 && xval < 4 && xval > -4)){
+
+    analogWrite(EN_A, speed3);  //for controlling the speed of the motor
+
+    digitalWrite(INP_A1, LOW); //for controlling the direction of the motor
+    digitalWrite(INP_A2, HIGH); 
+    
+    analogWrite(EN_B, speed3); //for controlling the speed of the motor
+
+    digitalWrite(INP_B1, HIGH); //for controlling the direction of the motor
+    digitalWrite(INP_B2, LOW); 
+
+  }else if ((yval==4 && xval < 4 && xval > -4)){
+
+    analogWrite(EN_A, speed4);  //for controlling the speed of the motor
+
+    digitalWrite(INP_A1, LOW); //for controlling the direction of the motor
+    digitalWrite(INP_A2, HIGH); 
+    
+    analogWrite(EN_B, speed4); //for controlling the speed of the motor
+
+    digitalWrite(INP_B1, HIGH); //for controlling the direction of the motor
+    digitalWrite(INP_B2, LOW); 
+
+  }else if ((yval==5 && xval < 4 && xval > -4)){
+
+    analogWrite(EN_A, speed5);  //for controlling the speed of the motor
+
+    digitalWrite(INP_A1, LOW); //for controlling the direction of the motor
+    digitalWrite(INP_A2, HIGH); 
+    
+    analogWrite(EN_B, speed5); //for controlling the speed of the motor
+
+    digitalWrite(INP_B1, HIGH); //for controlling the direction of the motor
+    digitalWrite(INP_B2, LOW); 
+
+  }else if ((yval==6 && xval < 4 && xval > -4)){
+
+    analogWrite(EN_A, speed6);  //for controlling the speed of the motor
+
+    digitalWrite(INP_A1, LOW); //for controlling the direction of the motor
+    digitalWrite(INP_A2, HIGH); 
+    
+    analogWrite(EN_B, speed6); //for controlling the speed of the motor
+
+    digitalWrite(INP_B1, HIGH); //for controlling the direction of the motor
+    digitalWrite(INP_B2, LOW); 
+
+  }else if ((yval==7 && xval < 4 && xval > -4)){
+
+    analogWrite(EN_A, speed7);  //for controlling the speed of the motor
+
+    digitalWrite(INP_A1, LOW); //for controlling the direction of the motor
+    digitalWrite(INP_A2, HIGH); 
+    
+    analogWrite(EN_B, speed7); //for controlling the speed of the motor
+
+    digitalWrite(INP_B1, HIGH); //for controlling the direction of the motor
+    digitalWrite(INP_B2, LOW); 
+  }
+
+
+
+}
+
